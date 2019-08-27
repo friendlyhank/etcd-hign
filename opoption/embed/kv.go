7aca73d84facdf3d0ce821937e797641307a46e7
Hank <853160447@qq.com>
2019-08-27 16:48:53 +0800

    kv interface,opResponse

--- opoption/embed/kv.go
+++ opoption/embed/kv.go
@@ -3,19 +3,100 @@ package embed
 import (
 	"context"
 	"fmt"
+	"net"
 )
 
-type Kv struct{
+
+
+type KV interface{
+	Put(ctx context.Context,key,val string,opts ...OpOption)(*PutResponse, error)
+	Get(ctx context.Context, key string, opts ...OpOption) (*GetResponse, error)
+	Delete(ctx context.Context, key string, opts ...OpOption) (*DeleteResponse, error)
+	Do(ctx context.Context, op Op) (OpResponse, error)
 }
 
-func (kv *Kv) Put(ctx context.Context, key, val string, opts ...OpOption) error {
-	op := &Op{}
+type Lease interface {
+}
+
+type Watcher interface {
+}
+
+//set pb
+type PutResponse struct{}
+type GetResponse struct{}
+type DeleteResponse struct{}
+
+
+/*总分总形式，方法聚合有可以分离
+Put、Get、Delete都是用Do,结构返回统一用OpResponse,又可以获取具体的，如Put put := op.Put()
+*/
+type OpResponse struct{
+	put *PutResponse
+	get *GetResponse
+	del *DeleteResponse
+}
+
+func (op OpResponse)Put()*PutResponse{return op.put}
+func (op OpResponse)Get()*GetResponse{return op.get}
+func (op OpResponse)Del()*DeleteResponse{return op.del}
+
+func (resp *PutResponse)OpResponse()OpResponse{
+	return OpResponse{put:resp}
+}
+
+func (resp *GetResponse)OpResponse()OpResponse{
+	return OpResponse{get:resp}
+}
+
+func (resp *DeleteResponse)OpResponse()OpResponse{
+	return OpResponse{del:resp}
+}
+
+//Clinet 结构体
+type Client struct{
+	KV
+	Lease
+	Watcher
+	Conn *net.Conn
+}
+
+
+/*提供入口,api接口
+*/
+func NewKV(c *Client) KV {
+	api := &Kv{Conn:c.Conn}
+	return api
+}
+
+type Kv struct{
+	Conn *net.Conn
+}
+
+func (kv *Kv)Get(ctx context.Context, key string, opts ...OpOption) (*GetResponse, error){
+	return nil,nil
+}
+
+func (kv *Kv) Put(ctx context.Context, key, val string, opts ...OpOption)(*PutResponse, error) {
+	//注意不是引用类型传入了引用类型的函数
+	op := Op{t:tPut}
 	op.applyOpts(opts)
 
-	kv.Do(ctx,op)
-	return nil
+	r,err := kv.Do(ctx,op)
+
+	return r.put,err
 }
 
-func (kv *Kv)Do(ctx context.Context,op *Op){
-	fmt.Println(op.LeaseID)
+func (kv *Kv)Delete(ctx context.Context, key string, opts ...OpOption) (*DeleteResponse, error){
+	return nil,nil
+}
+
+func (kv *Kv)Do(ctx context.Context, op Op) (OpResponse, error){
+	//判断类型
+	switch op.t {
+	case tPut:
+		fmt.Println(op.LeaseID)
+		return OpResponse{put:&PutResponse{}},nil
+	}
+
+	return OpResponse{},nil
 }
--- opoption/embed/op.go
+++ opoption/embed/op.go
@@ -1,11 +1,25 @@
 package embed
 
+import "fmt"
+
 /**
  *op etcd比较常见的写法
  *适用于可选条件，且参数比较多的时候可以用
  *但不适用于同时用多个方法(可读性差)
  */
+
+type opType int
+
+const (
+	// A default Op has opType 0, which is invalid.
+	tRange opType = iota + 1
+	tPut
+	tDeleteRange
+	tTxn
+)
+
 type Op struct{
+	t   opType //操作类型
 	LeaseID int64
 }
 
@@ -17,6 +31,7 @@ func WithLease(leaseID int64) OpOption{
 
 
 func (o *Op)applyOpts(ops []OpOption){
+	fmt.Println(o)
 	for _,opOption := range ops{
 		opOption(o)
 	}
