9039b8065c372b90d97886ea975eb23d60dc5fa8
Hank <853160447@qq.com>
2020-11-07 11:23:33 +0800

    test pipeline

---
+++ net/server/etcdserver/api/rafthttp/http_test.go
@@ -0,0 +1,91 @@
+package rafthttp
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"go.uber.org/zap"
+
+	"github.com/friendlyhank/etcd-hign/net/pkg/pbutil"
+	"github.com/friendlyhank/etcd-hign/net/pkg/types"
+	"github.com/friendlyhank/etcd-hign/net/raft/raftpb"
+	"github.com/friendlyhank/etcd-hign/net/server/etcdserver/api/version"
+)
+
+func TestServeRaftPrefix(t *testing.T) {
+	testCases := []struct {
+		method    string
+		body      io.Reader
+		p         Raft
+		clusterID string
+
+		wcode int
+	}{
+		{
+			// bad method
+			"GET",
+			bytes.NewReader(pbutil.MustMarshal(&raftpb.Message{})),
+			&fakeRaft{},
+			"0",
+			http.StatusMethodNotAllowed,
+		},
+		{
+			// bad method
+			"PUT",
+			bytes.NewReader(pbutil.MustMarshal(&raftpb.Message{})),
+			&fakeRaft{},
+			"0",
+			http.StatusMethodNotAllowed,
+		},
+		{
+			// bad method
+			"DELETE",
+			bytes.NewReader(pbutil.MustMarshal(&raftpb.Message{})),
+			&fakeRaft{},
+			"0",
+			http.StatusMethodNotAllowed,
+		},
+		{
+			//bad method
+			"POST",
+			&errReader{}, //io读取错误
+			&fakeRaft{},
+			"0",
+			http.StatusBadRequest,
+		},
+	}
+
+	for i, tt := range testCases {
+		req, err := http.NewRequest(tt.method, "foo", tt.body)
+		if err != nil {
+			t.Fatalf("#%d: could not create request: %#v", i, err)
+		}
+		req.Header.Set("X-Etcd-Cluster-ID", tt.clusterID)
+		req.Header.Set("X-Server-Version", version.Version)
+		rw := httptest.NewRecorder()
+		h := newPipelineHandler(&Transport{Logger: zap.NewExample()}, tt.p, types.ID(0))
+
+		// goroutine because the handler panics to disconnect on raft error
+		donec := make(chan struct{})
+		go func() {
+			defer func() {
+				recover()
+				close(donec)
+			}()
+			h.ServeHTTP(rw, req) //可以这样写
+		}()
+		<-donec
+		if rw.Code != tt.wcode {
+			t.Errorf("#%d: got code=%d, want %d", i, rw.Code, tt.wcode)
+		}
+	}
+}
+
+// errReader implements io.Reader to facilitate a broken request.
+type errReader struct{}
+
+func (er *errReader) Read(_ []byte) (int, error) { return 0, errors.New("some error") }
